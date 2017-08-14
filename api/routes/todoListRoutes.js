d51c948bd71ab5afd97ed5c14dc52d8209f58506
Przemysław Golec <golec.przemyslaw@gmail.com>
2017-08-14 13:40:36 +0200

    ES6 improvements and docker containers for both node and mongo

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -1,6 +1,6 @@
 'use strict'
 
-module.exports = function(app) {
+module.exports = (app) => {
   const todoList = require('../controllers/todoListController')
 
   app.route('/tasks')
--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-var express = require('express'),
+const express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
   Task = require('./api/models/todoListModel'),
-  bodyParser = require('body-parser');
+  bodyParser = require('body-parser')
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb');
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://mongo/Tododb')
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-app.use(function(req, res) {
+app.use((req, res, next) => {
+  console.log('Request type: ', req.method)
+  // console.log(req.body)
+  next()
+})
+
+const routes = require('./api/routes/todoListRoutes')
+routes(app)
+
+app.use((req, res) => {
   res.status(404).send({url: req.originalUrl + ' not found'})
-});
+})
 
-var routes = require('./api/routes/todoListRoutes');
-routes(app);
+app.listen(port)
 
-app.listen(port);
-
-console.log('todo list RESTful API server started on: ' + port);
+console.log('todo list RESTful API server started on: ' + port)
